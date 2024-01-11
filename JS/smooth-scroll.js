1a8fce1d12fb01e22839074ee188d732caec7b4b
Samarium <70806611+samarium224@users.noreply.github.com>
2024-01-11 21:51:50 +0600

    smooth-scroll-added

---
+++ JS/smooth-scroll.js
@@ -0,0 +1,26 @@
+ScrollReveal().reveal('.about_us', {
+    delay: 500,
+    duration: 800,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('#our_services', {
+    delay: 500,
+    duration: 1500,
+    easing: 'ease-in',
+    distance: '150px'
+});
+
+ScrollReveal().reveal('.project_idea', {
+    delay: 400,
+    duration: 1500,
+    easing: 'ease-in',
+    distance: '250px'
+});
+
+ScrollReveal().reveal('.contect_us', {
+    delay: 800,
+    duration: 1200,
+    easing: 'ease-in',
+    distance: '150px'
+});
